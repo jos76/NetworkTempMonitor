7cb507b9a1eafd295280ac35b3107a19b0b9aa20
Jon Savage <jhsavage76@gmail.com>
2017-01-21 13:11:57 -0500

    changed server url and ser port

--- read_temp.py
+++ read_temp.py
@@ -1,11 +1,11 @@
 import serial
 import requests
 
-ser_port = '/dev/cu.usbmodem14111' # macbook ser port
-#ser_port = '/dev/ttyAMA0' # pi ser port
+#ser_port = '/dev/cu.usbmodem14111' # macbook ser port
+ser_port = '/dev/ttyAMA1' # pi ser port
 ser = serial.Serial(ser_port, 9600)
 
-server_url = 'http://127.0.0.1:3000/temp_data'
+server_url = 'http://192.168.1.147:3000/temp_data'
 
 num_reads = 0
 totalC = 0
@@ -19,7 +19,7 @@ while True:
     totalC = totalC + float(values[1])
     # third is temp in F
     totalF = totalF + float(values[2])
-    if num_reads >= 10:
+    if num_reads >= 60:
         avgC = totalC / num_reads
         avgF = totalF / num_reads
         body = {"avgF": int(round(avgF)), "avgC": int(round(avgC))}
