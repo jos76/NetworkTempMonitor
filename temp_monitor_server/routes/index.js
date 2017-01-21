1db02a8e5bcf61a73fd625e282d05a5a3bbf1d37
Jon Savage <jhsavage76@gmail.com>
2017-01-21 13:39:13 -0500

    added timestamp to temp view

--- temp_monitor_server/routes/index.js
+++ temp_monitor_server/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
-var lastTempData = {"avgF": 70, "avgC": 21}
+var lastTempData = {"avgF": 70, "avgC": 21, "timestamp": moment().format("MM-dd-yyyy h:mm:ss a")}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/temp_data', function(req, res, next) {
   lastTempData = req.body;
+  lastTempData.timestamp = moment().format("MM-dd-yyyy h:mm:ss a");
   res.end();
 })
 
