ecdc5c11963774f537d750043558e0ee823209eb
Jon Savage <jhsavage76@gmail.com>
2017-01-21 13:26:55 -0500

    checked for values in read serial, update ser port for pi

--- read_temp.py
+++ read_temp.py
@@ -2,7 +2,7 @@ import serial
 import requests
 
 #ser_port = '/dev/cu.usbmodem14111' # macbook ser port
-ser_port = '/dev/ttyAMA1' # pi ser port
+ser_port = '/dev/ttyACM0' # pi ser port
 ser = serial.Serial(ser_port, 9600)
 
 server_url = 'http://192.168.1.147:3000/temp_data'
@@ -12,22 +12,23 @@ totalC = 0
 totalF = 0
 while True:
     ser_data = ser.readline()
-    num_reads = num_reads + 1
     values = ser_data.split('|')
-    # first val is voltage
-    # second is temp in C
-    totalC = totalC + float(values[1])
-    # third is temp in F
-    totalF = totalF + float(values[2])
-    if num_reads >= 60:
-        avgC = totalC / num_reads
-        avgF = totalF / num_reads
-        body = {"avgF": int(round(avgF)), "avgC": int(round(avgC))}
-        try:
-            requests.post(server_url, data=body)
-        except:
-            print('Failed to send POST message')
-        # print (body)
-        num_reads = 0
-        totalC = 0
-        totalF = 0
+    if len(values) >= 3:
+        num_reads = num_reads + 1
+        # first val is voltage
+        # second is temp in C
+        totalC = totalC + float(values[1])
+        # third is temp in F
+        totalF = totalF + float(values[2])
+        if num_reads >= 60:
+            avgC = totalC / num_reads
+            avgF = totalF / num_reads
+            body = {"avgF": int(round(avgF)), "avgC": int(round(avgC))}
+            try:
+                requests.post(server_url, data=body)
+            except:
+                print('Failed to send POST message')
+            # print (body)
+            num_reads = 0
+            totalC = 0
+            totalF = 0
