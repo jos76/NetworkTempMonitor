b8909d67699fd66b8eee2366fd82d88cba94eef8
Jon Savage <jhsavage76@gmail.com>
2017-01-21 13:42:13 -0500

    fixed timestamp format

--- temp_monitor_server/routes/index.js
+++ temp_monitor_server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var moment = require('moment');
 var router = express.Router();
 
-var lastTempData = {"avgF": 70, "avgC": 21, "timestamp": moment().format("MM-dd-yyyy h:mm:ss a")}
+var lastTempData = {"avgF": 70, "avgC": 21, "timestamp": moment().format("MM-DD-YY h:mm:ss a")}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/temp_data', function(req, res, next) {
   lastTempData = req.body;
-  lastTempData.timestamp = moment().format("MM-dd-yyyy h:mm:ss a");
+  lastTempData.timestamp = moment().format("MM-DD-YY h:mm:ss a");
   res.end();
 })
 
